1e88c20cd19c63c4ca92818a061a7d5708a81988
pholophus <iqbalkisas6@gmail.com>
2024-08-25 10:57:37 +0800

    first commit

---
+++ api/api.go
@@ -0,0 +1,16 @@
+package api
+
+type CoinBalanceParams struct {
+	Username string
+}
+
+type CoinBalanceResponse struct {
+	Code int
+
+	Balance int64
+}
+
+type Error struct {
+	Code    int
+	Message string
+}
